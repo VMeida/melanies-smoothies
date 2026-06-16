4ea74eb1f17c7430228fdc13aa7309f8e4d20c10
Victor Almeida <vm.almeida9@gmail.com>
2026-06-16 17:18:54 -0300

    Remove unused smoothie API call and related code

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,8 +31,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-    # st.write(ingredients_string)
+        st.subheader(fruitchosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ 
         insert into smoothies.public.orders(ingredients, name_on_order)
@@ -46,6 +47,4 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
